3f9de3f310a7a3baaec9854b4e982b85e838fb49
gennttllee <udooto72@gmail.com>
2022-08-07 04:05:42 +0100

    am fed up of curs

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
+/* https://gennttllee.herokuapp.com */
 
-const API = axios.create({baseURL :'https://gennttllee.herokuapp.com'})
+const API = axios.create({ baseURL: 'http://localhost:5000' })
 
-API.interceptors.request.use((req)=> {
-    if (localStorage.getItem('user')){
+API.interceptors.request.use((req) => {
+    if (localStorage.getItem('user')) {
         const token = JSON.parse(localStorage.getItem('user')).token
         req.headers.authorization = `Bearer ${token}`;
         return req;
@@ -13,18 +14,18 @@ API.interceptors.request.use((req)=> {
     return req;
 })
 
-export const fetchData =()=> API.get('/posts');
-export const createPost =(newPost)=> API.post('/posts', newPost);
-export const updatePost =(id, post) => API.patch(`/posts/${id}`, post);
+export const fetchData = () => API.get('/posts');
+export const createPost = (newPost) => API.post('/posts', newPost);
+export const updatePost = (id, post) => API.patch(`/posts/${id}`, post);
 export const deletePost = (id) => API.delete(`/posts/${id}`);
-export const likePost =(id, data)=> API.patch(`/posts/${id}/likePost`, data);
-export const commentPost =(data, id)=> API.patch(`/posts/${id}/commentPost`, data);
-export const searchPost =(id)=> API.get(`/posts/${id}`);
-export const search =(id)=> API.get(`/posts/${id}/search`);
+export const likePost = (id, data) => API.patch(`/posts/${id}/likePost`, data);
+export const commentPost = (data, id) => API.patch(`/posts/${id}/commentPost`, data);
+export const searchPost = (id) => API.get(`/posts/${id}`);
+export const search = (id) => API.get(`/posts/${id}/search`);
 
-export const signIn =(data)=> API.post('/user/signIn', data);
-export const resetPass =(email)=> API.get(`user/reset/${email}`)
-export const savePass =(data)=> API.patch(`user/reset/${data.email}`, data)
-export const signUp =(data)=> API.post('/user/signUp', data);
-export const searchProfile =(id)=> API.get(`/user/${id}/profile`, id);
-export const google =(data)=> API.post('/user/google', data)
+export const signIn = (data) => API.post('/user/signIn', data);
+export const resetPass = (email) => API.get(`user/reset/${email}`)
+export const savePass = (data) => API.patch(`user/reset/${data.email}`, data)
+export const signUp = (data) => API.post('/user/signUp', data);
+export const searchProfile = (id) => API.get(`/user/${id}/profile`, id);
+export const google = (data) => API.post('/user/google', data)
